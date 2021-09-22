5e5d30c80d65ec2f803271bb1095fa21021552ee
sukima <inthesukima@gmail.com>
2021-09-22 11:37:19 +0000

    3-3

--- ch3/main.cpp
+++ ch3/main.cpp
@@ -1,7 +1,10 @@
 int main()
 {
-	std::cout << "one "s ;
-	std::cout << "two "s ;
-	std::cout << "three "s;
-}
+	auto answer = 42;
+	std::cout << answer << "\n"s;
+	auto pi = 3.14;
+	std::cout << pi << "\n"s;
 
+	auto question = "Lite, the Universe, and Everything."s;
+	std::cout << question ;
+}
