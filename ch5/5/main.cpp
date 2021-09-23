3d3196f9bb7377919e46377587a8f20513058d4d
sukima <inthesukima@gmail.com>
2021-09-23 11:14:08 +0000

    ch5_5_8

---
+++ ch5/5/main.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+	double temp = 36.6 ;
+	
+	if ( ( temp >= 36.1)&&(temp <= 37.2))
+	{std::cout << "Good\n"s ;}
+	else
+	{std::cout << "Bad\n"s ;}
+}
---
+++ ch5/7/main.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+	auto x = 1.7 ;
+
+	if(x <= 1.1||x >= 1.9)
+	{std::cout << "no\n"s ;}
+	else
+	{std::cout << "yes\n"s ;}
+}
