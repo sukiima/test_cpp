aebc51abcc3a208f681620d4a6c007e4a7ee4e5f
sukima <inthesukima@gmail.com>
2021-09-29 12:02:12 +0000

    hon-std::

--- ch16/13/main.cpp
+++ ch16/13/main.cpp
@@ -1,20 +1,19 @@
-auto none_of = [](auto first, auto last, auto pred)
+auto none_of = []( auto first, auto last, auto pred )
 {
-	for(auto iter = first; first != last ; ++iter)
-	{
-		if(pred(*iter))
-			return false ;
-	}
-	return true ;
+    for ( auto iter = first ; first != last ; ++iter )
+    {
+        if ( pred(*iter) )
+            return false ;
+    }
+    return true ;
 } ;
 
 int main()
 {
-	std::vector<int> v = {1, 2, 3, 4, 5} ;
+    std::vector<int> v = {1,2,3,4,5} ;
 
-	auto is_100 = [](auto x){return x == 100 ;} ;
+    // 値は100か？
+    auto is_100 = [](auto x){ return x == 100 ; } ;
 
-	bool b = none_of(std::begin(v), std::end(v), is_100) ;
-
-	std::cout << b ;
+    bool b = none_of( std::begin(v), std::end(v), is_100 ) ;
 }
